d7ceb1e436dc3ff4492df68cee4978665aa2aaf2
saravananradhakrishnan <raghavendrasaravanan@gmail.com>
2017-11-16 12:09:16 +0530

    changes for postgres

--- db/migrate/20171111135746_create_users.rb
+++ db/migrate/20171111135746_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :name
       t.string :mobile
-      t.boolean :is_admin,:default => 0
+      t.boolean :is_admin,:default => false
 
       t.timestamps
     end
